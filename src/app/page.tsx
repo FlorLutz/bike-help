675f1d39578cc8d2df63c332420531d345ffb610
Florian Lutz <florian.lutz@ymail.com>
2024-04-10 17:37:51 +0200

    add burger menu and placeholders for sub pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
+"use client";
+
 import Layout from "../components/Layout/Layout";
-import Image from "next/image";
+import React from "react";
+import Map from "react-map-gl/dist/esm/components/map";
 
 export default function Home() {
   return (
     <Layout>
       <main>
-        <Image src="/../../public/map.png" width={500} height={500} alt="map" />
+        MAP PLACEHOLDER
+        {/* <Map
+          mapboxAccessToken="<Mapbox access token>"
+          initialViewState={{
+            longitude: -122.4,
+            latitude: 37.8,
+            zoom: 14,
+          }}
+          style={{ width: 600, height: 400 }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+        /> */}
       </main>
     </Layout>
   );
