ad6a0dd9125c26b44070a217490fda241406e196
Florian Lutz <florian.lutz@ymail.com>
2024-04-15 22:19:03 +0200

    update request model

--- db/models/Request.ts
+++ db/models/Request.ts
@@ -4,7 +4,10 @@ const { Schema } = mongoose;
 const RequestSchema = new Schema({
   problem: { type: String, required: true },
   description: { type: String, required: false },
+  locationdetails: { type: String, required: false },
   tools: { type: String, required: false },
+  date: { type: Date, required: true },
+  isOpen: { type: Boolean, required: true },
 });
 const Request =
   mongoose.models.Request || mongoose.model("Request", RequestSchema);
